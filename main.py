08fa4761cf7bfaef547984a7d69d34d453d63200
Binta F <45748934+NabintouSFofana@users.noreply.github.com>
2022-11-05 20:50:18 -0500

    update2

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from tabulate import tabulate
 
 ingredients = ['chicken', 'egg', 'beef', 'fish', 'onion', 'tomato', 'garlic', 'bell_pepper', 'rice', 'pasta', 'bread']
-recipes = ['chicken & rice', 'fried beef & rice with vegetables', 'tomato pasta & meat ball', 'fried eggs & bread',
+recipes = ['chicken & rice', 'fried beef & rice with vegetables', 'tomato pasta & meat(fish or chicken) ball',
+           'fried eggs & bread',
            'fried fish with potatoes']
-chicken = rice = [recipes[0]]
-beef = rice = garlic = [recipes[1]]
-tomato = onion = pasta = bread = garlic = [recipes[2]]
-egg = bread = [recipes[3]]
-fish = [recipes[4]]
+
+chicken, beef, any_meat, egg, fish = recipes
+
 selected_ingredient = []
 selected_meal = []
 
@@ -79,35 +78,159 @@ class Meals:
                         all_ingredients = ingredient1 + ", " + ingredient2 + ", " + ingredient3 + " and " + ingredient4
 
                         print(f"\nOk,you have {all_ingredients}.")
-                        print("\nHere are some meals you can cook")
-                        f = open("meal.txt", "w")
-                        f = open("meal.txt", "a")
-                        mydata = [
-                            [f"{ingredient1}", f"{recipes}"]
-                        ]
-                        head = ["Ingredients", "Meal ideas"]
-                        f.write(
-                            tabulate(mydata, headers=head, tablefmt="grid"))
-                        f = open("meal.txt", "r")
-                        print(f.read())
-                        f.close()
-                        with open('cooking_instructions.txt') as f:
-                            contents = f.read()
-                            print(contents)
-                            return 'Good! See you soon'
+
+                        if 'chicken' in all_ingredients:
+                            print("\nHere are some meals you can cook:")
+                            print(f"\n{chicken}, and {any_meat}")
+
+                            f = open("meal.txt", "w")
+                            f = open("meal.txt", "a")
+
+                            mydata = [
+                                [f"{all_ingredients}", f"{chicken}, and {any_meat}"]
+                            ]
+                            head = ["Ingredients", "Meal ideas"]
+                            f.write(
+                                tabulate(mydata, headers=head, tablefmt="grid"))
+                            print("\ncheck the file 'meal' and 'cooking_instructions' and follow the instruction ")
+                            break
+
+                        elif 'fish' in all_ingredients:
+                            print("\nHere are some meals you can cook:")
+                            print(f"\n{fish}, and {any_meat}")
+
+                            f = open("meal.txt", "w")
+                            f = open("meal.txt", "a")
+
+                            mydata = [
+                                [f"{all_ingredients}", f"{fish}, and {any_meat}"]
+                            ]
+                            head = ["Ingredients", "Meal ideas"]
+                            f.write(
+                                tabulate(mydata, headers=head, tablefmt="grid"))
+                            print("\ncheck the file 'meal' and 'cooking_instructions' and follow the instruction ")
+                            break
+
+                        elif 'beef' in all_ingredients:
+                            print("\nHere are some meals you can cook:")
+                            print(f"\n{beef}, and {any_meat}")
+
+                            f = open("meal.txt", "w")
+                            f = open("meal.txt", "a")
+
+                            mydata = [
+                                [f"{all_ingredients}", f"{beef}, and {any_meat}"]
+                            ]
+                            head = ["Ingredients", "Meal ideas"]
+                            f.write(
+                                tabulate(mydata, headers=head, tablefmt="grid"))
+                            print("\ncheck the file 'meal' and 'cooking_instructions' and follow the instruction ")
+                            break
+
+                        elif 'egg' in all_ingredients:
+                            print("\nHere are some meals you can cook:")
+                            print(egg)
+
+                            f = open("meal.txt", "w")
+                            f = open("meal.txt", "a")
+
+                            mydata = [
+                                [f"{all_ingredients}", f"{egg}"]
+                            ]
+                            head = ["Ingredients", "Meal ideas"]
+                            f.write(
+                                tabulate(mydata, headers=head, tablefmt="grid"))
+                            print("\ncheck the file 'meal' and 'cooking_instructions' and follow the instruction ")
+                            break
+
+                        else:
+                            print('try again please')
+
                     else:
                         print("Try again please")
                         return meal.ingredient()
                 break
 
+        return 'ENJOY YOUR MEAL'
+
+    def fish_meal(self):
+        for x in range(4):
+            print("enter a protein")
+            console = input('>')
+            for item in recipes:
+                if console == 'fish':
+                    return fish, any_meat
+                break
+            print('try again please')
+
+    def beef_meal(self):
+        for x in range(4):
+            print("enter a protein")
+            console = input('>')
+            for item in recipes:
+                if console == 'beef':
+                    return beef, any_meat
+                break
+            print('try again please')
+
+    def chicken_meal(self):
+        for x in range(4):
+            print("enter a protein")
+            console = input('>')
+            for item in recipes:
+                if console == 'chicken':
+                    return chicken, any_meat
+                break
+            print('try again please')
+
+    '''
+
+    def chicken2(self):
+        print("\nHere are some meals you can cook")
+        print(f"{chicken}, and {any_meat}")
+
+        f = open("meal.txt", "w")
+        f = open("meal.txt", "a")
+
+        mydata = [
+            [f"{ingredients}", f"{chicken}, and {any_meat}"]
+        ]
+        head = ["Ingredients", "Meal ideas"]
+        f.write(
+            tabulate(mydata, headers=head, tablefmt="grid"))
+        print("check the file 'meal' and 'cooking_instructions' and follow the instruction ")
+   '''
+
+
+'''
+ with open('cooking_instructions.txt') as f:
+                contents = f.read()
+                print(contents)
+                return 'Good! See you soon'
+'''
+
+
+def egg_meal(self):
+    for x in range(4):
+        print("enter a protein")
+        console = input('>')
+        for item in recipes:
+            if console == 'egg':
+                return egg
+            break
+        print('try again please')
+
+
+'''
     def recipes(self):
 
         for item in recipes:
             i = 0
-            for i in range(5):
+            for i in range(1):
                 print(item)
                 i += 1
                 break
+'''
 
 
 def shop(self):
@@ -129,3 +252,25 @@ meal = Meals()
 print(meal.ingredient())
 
 # print(meal.recipe())
+
+# print(meal.fish_meal())
+
+'''
+for item in recipes:
+    print("enter a protein")
+    console = input('>')
+    if console == 'chicken':
+        print(f"{chicken}, and {any_meat}")
+        break
+    elif console == 'fish':
+        print(f"{fish}, and {any_meat}")
+        break
+    elif console == 'beef':
+        print(f"{beef}, and {any_meat}")
+        break
+    elif console == 'egg':
+        print(egg)
+        break
+    else:
+        print('try again please')
+'''
